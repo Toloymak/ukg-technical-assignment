b2785f7abb4296d673fe720da9076191681a95a8
Alexey Brovko <brovko-a@live.com>
2025-03-31 18:54:32 +0300

    Migrations

---
+++ src/UKG.HCM/Infrastructure/Migrations/20250331144814_AddRolesAndAccounts.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UKG.HCM.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRolesAndAccounts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Accounts",
+                newName: "PasswordHash");
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AccountRoles",
+                columns: table => new
+                {
+                    AccountDalAccountId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RolesName = table.Column<string>(type: "nvarchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountRoles", x => new { x.AccountDalAccountId, x.RolesName });
+                    table.ForeignKey(
+                        name: "FK_AccountRoles_Accounts_AccountDalAccountId",
+                        column: x => x.AccountDalAccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "AccountId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AccountRoles_Roles_RolesName",
+                        column: x => x.RolesName,
+                        principalTable: "Roles",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountRoles_RolesName",
+                table: "AccountRoles",
+                column: "RolesName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccountRoles");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Accounts",
+                newName: "Password");
+        }
+    }
+}
---
+++ src/UKG.HCM/Infrastructure/Migrations/20250331145941_AddDefaultRoles.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UKG.HCM.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDefaultRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Roles",
+                column: "Name",
+                values: new object[]
+                {
+                    "EMPLOYEE",
+                    "HR ADMIN",
+                    "MANAGER"
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Name",
+                keyValue: "EMPLOYEE");
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Name",
+                keyValue: "HR ADMIN");
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Name",
+                keyValue: "MANAGER");
+        }
+    }
+}
